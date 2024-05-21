08d635d9edb9c29fae43f8ed695737931ef8f5d6
Luca Salvarani <lucasalvarani99@gmail.com>
2024-05-21 17:18:30 +0200

    fix: Fix circular import

--- jycli/components/_renderables.py
+++ jycli/components/_renderables.py
@@ -2,15 +2,13 @@
 import sys as _sys
 from xml.sax.saxutils import escape
 
-from jycli.console import Console
-
 class Renderable:
     """ Base class for all renderable elements. """
     def __console_print__(self, console):
-        # type: (Console) -> str
         raise NotImplementedError("The '%s' method must be defined when subclassing the 'Renderable' class." % _sys._getframe().f_code.co_name)
     
     def __str__(self):
+        from jycli.console import Console
         return self.__console_print__(
             console=Console(),
         )
